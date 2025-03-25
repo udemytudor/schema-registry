7c4fb1f44e10f737a7cedfe1e10693716373c195
TudorBatica <tudor.batica@gmail.com>
2025-03-25 14:47:32 +0200

    init producer code

---
+++ code/schemas/src/main/java/com/example/schemas/SchemasApplication.java
@@ -0,0 +1,22 @@
+package com.example.schemas;
+
+import com.streaming.platform.UserActivityEvent;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class SchemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemasApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner runner(KafkaTemplate<String, UserActivityEvent> template) {
+		var event = UserActivityEvent.newBuilder().setUserId(123).setActivity("logged in").build();
+		return args -> template.send("demo", event);
+	}
+}
---
+++ code/schemas/src/test/java/com/example/schemas/SchemasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
